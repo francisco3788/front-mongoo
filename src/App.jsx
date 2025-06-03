ac4d6541e7c263a63cdc57f55a868bcd4b6b4846
francisco3788 <francisco.molinac@campusucc.edu.co>
2025-06-03 02:38:44 -0500

    correccion errores graficas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Graficas from "./components/Graficas";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/graficas" element={<Graficas />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/graficas" element={<Graficas />} />
+    </Routes>
   );
 }
